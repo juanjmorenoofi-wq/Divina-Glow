71cfe47ba97fefb9ac2dfc4bd4524af9d620fa26
juanjmorenoofi-wq <juanj.moreno.ofi@gmail.com>
2026-06-01 16:44:56 -0500

    head

---
+++ src/components/head.jsx
@@ -0,0 +1,15 @@
+export function Head() {
+  return (
+    <header className="header">
+      <div className="conten-logo">
+        <img className="img-logo" src="tienda.webp" alt="" />
+        <h1 className="logo">Tienda online</h1>
+      </div>
+      <nav>
+        <a href="">Inicio</a>
+        <a href="">Servicios</a>
+        <a href="">Contacto</a>
+      </nav>
+    </header>
+  );
+}
