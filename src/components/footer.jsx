ecdf1de1bf24f8b39c1c1843bc347d5067ecfd1c
juanjmorenoofi-wq <juanj.moreno.ofi@gmail.com>
2026-07-16 15:40:15 -0500

    actualizacion-footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,27 +3,31 @@ export function Footer() {
     <>
       <div className="footer">
         <h2>Divina Glow</h2>
-        <div className="barra">
-          <a href="">Inicio</a>
-          <a href="">Producto</a>
-          <a href="">Servicio</a>
-          <a href="">Contacto</a>
-        </div>
-        <div className="info">
-          <div className="ubicacion">
-            <h1>hola</h1>
+        <div className="flex">
+          <div className="barra">
+            <a href="">Inicio</a>
+            <a href="">Producto</a>
+            <a href="">Servicio</a>
+            <a href="">Contacto</a>
           </div>
-          <div className="contacto">
-            <a href="">WhatsApp</a>
-            <br />
-            <br />
-            <a href="">TikTok</a>
-            <br />
-            <br />
-            <a href="">Instagram</a>
-            <br />
-            <br />
-            <a href="">Faceboock</a>
+        </div>
+        <div className="flex">
+          <div className="info">
+            <div className="ubicacion">
+              <h1>hola</h1>
+            </div>
+            <div className="contacto">
+              <a href="">WhatsApp</a>
+              <br />
+              <br />
+              <a href="">TikTok</a>
+              <br />
+              <br />
+              <a href="">Instagram</a>
+              <br />
+              <br />
+              <a href="">Faceboock</a>
+            </div>
           </div>
         </div>
       </div>
