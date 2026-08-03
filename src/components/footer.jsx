db154a605bceff91b5e2b26520efe308fe8eaeb7
juanjmorenoofi-wq <juanj.moreno.ofi@gmail.com>
2026-08-03 14:24:45 -0500

    imagenes-logos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -22,20 +22,38 @@ export function Footer() {
             <div className="contacto">
               <a href="">
                 <img
-                  src="https://tecnowasap.com/wp-content/uploads/2021/07/whatsapp-rosa.png"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/960px-WhatsApp.svg.png"
                   alt="imagen"
                 />
                 WhatsApp
               </a>
               <br />
               <br />
-              <a href="">TikTok</a>
+              <a href="">
+                <img
+                  src="https://cdn.pixabay.com/photo/2021/06/15/12/28/tiktok-6338432_960_720.png"
+                  alt="imagen"
+                />
+                TikTok
+              </a>
               <br />
               <br />
-              <a href="">Instagram</a>
+              <a href="">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Instagram_Glyph_Gradient_RGB_logo.svg/960px-Instagram_Glyph_Gradient_RGB_logo.svg.png"
+                  alt="imagen"
+                />
+                Instagram
+              </a>
               <br />
               <br />
-              <a href="">Faceboock</a>
+              <a href="">
+                <img
+                  src="https://png.pngtree.com/png-clipart/20230401/original/pngtree-facebook-icon-png-image_9015416.png"
+                  alt="imagen"
+                />
+                Faceboock
+              </a>
             </div>
           </div>
         </div>
