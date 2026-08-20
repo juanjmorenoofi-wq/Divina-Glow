959a9660edc72ecba23fc69d543a919c47f5eae6
juanjmorenoofi-wq <juanj.moreno.ofi@gmail.com>
2026-08-20 16:35:13 -0500

    metodos-de-pago

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -16,6 +16,8 @@ export function Footer() {
           <div className="info">
             <div className="ubicacion">
               <img className="img-logo" src="divina.webp" alt="" />
+              <br />
+              <br />
               <p>
                 Realzamos tu belleza con productos de calidad para cada ocacion
               </p>
@@ -50,12 +52,26 @@ export function Footer() {
             </div>
             <div className="contacto">
               <h3>Atencio al cliente</h3>
-              <a href="" className="imagen">
-                WhatsApp <a href="">3160746119</a>
-              </a>
+              <h3>
+                <iconos.Phone /> WhatsApp <br />
+                <a href="">3160746119</a>
+              </h3>
+              <h3>
+                <iconos.Mail />
+                Correo electronico <br />
+                <a href="">divinaglow@gmail.com</a>
+              </h3>
+              <h3>
+                <iconos.Clock /> Horario de atención
+                <br />
+                <a href="">
+                  lun-vie: 8:00Am - 6:00Pm <br />
+                  sab: 9:00Am - 2:00Pm
+                </a>
+              </h3>
             </div>
             <div className="sigue">
-              <h3>síguenos</h3>
+              <h3>Síguenos</h3>
               <a href="">
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Instagram_Glyph_Gradient_RGB_logo.svg/960px-Instagram_Glyph_Gradient_RGB_logo.svg.png"
@@ -94,6 +110,11 @@ export function Footer() {
                 /divinaglow
               </a>
             </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="pago">
+            <h3>metodos de pago</h3>
           </div>
         </div>
       </div>
